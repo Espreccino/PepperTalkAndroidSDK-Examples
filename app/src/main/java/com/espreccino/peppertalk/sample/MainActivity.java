90d0d871f506cb6c8f4981b9c4eb5b2bb369d7d8
Imran <imohammed23@gmail.com>
2015-03-17 14:04:20 +0530

    peppertalk version 0.4.8

--- app/src/main/java/com/espreccino/peppertalk/sample/MainActivity.java
+++ app/src/main/java/com/espreccino/peppertalk/sample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.espreccino.peppertalk.sample;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,16 +11,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.espreccino.peppertalk.JSONCallback;
 import com.espreccino.peppertalk.PepperTalk;
+import com.espreccino.peppertalk.PepperTalkError;
+import com.espreccino.peppertalk.UserCallback;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
-import java.util.UUID;
+import java.util.Set;
 
 
 public class MainActivity extends ActionBarActivity implements LoginFragment.LoginFragmentListener,
@@ -25,23 +41,22 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
 
     private final String TAG = "MainActivity.class";
 
-    static final String[] USERS = {"Jon:j_android@getpeppertalk.com",
-            "Ben:b_android@getpeppertalk.com"};
+    static final String[] USERS = {"Jon:j_android@getpeppertalk.com"};
 
-    static final String[] TOPICS = {"10011:Lets Ride!",
-            "10022:Lets Eat!"};
+    static final String[] TOPICS = {"10011:Lets Ride!"};
 
     static List<User> mUsers = new ArrayList<User>();
     static List<Topic> mTopics = new ArrayList<Topic>();
     private final static String PREF_USER = "com.espreccino.peppertalk.sample_user_login";
     private final static String PREF_USER_NAME = "com.espreccino.peppertalk.sample_user_name";
 
+    UsersFragment mUsersFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addSampleData();
         if (savedInstanceState == null) {
             String userId = getRegisteredUser();
             Fragment fragment;
@@ -51,26 +66,39 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
                 getSupportFragmentManager().beginTransaction()
                         .add(R.id.container, fragment)
                         .commit();
-            } else {
-                loadUserFragment(userId);
             }
         }
     }
 
-    private void addSampleData() {
-        if (mUsers.size() != USERS.length) {
-            for (String usr : USERS) {
-                String[] split = usr.split(":");
-                mUsers.add(new User(split[0], split[1]));
-            }
-        }
 
-        if (mTopics.size() != TOPICS.length) {
-            for (String topic : TOPICS) {
-                String[] split = topic.split(":");
-                mTopics.add(new Topic(split[0], split[1]));
-            }
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        loadDataFromPrefs();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        saveDataToSharedPrefs();
     }
 
     @Override
@@ -82,8 +110,8 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
     }
 
     @Override
-    public void onConnectionFailed(Throwable e) {
-        e.printStackTrace();
+    public void onConnectionFailed(PepperTalkError error) {
+        error.printStackTrace();
     }
 
     @Override
@@ -98,9 +126,12 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
             getSupportActionBar().show();
             setTitle("PepperTalk");
             initPepperTalk(userId);
+            if (mUsersFragment == null) {
+                mUsersFragment = UsersFragment.getInstance(getRegisteredUser());
+            }
             getSupportFragmentManager()
                     .beginTransaction()
-                    .replace(R.id.container, UsersFragment.getInstance(getRegisteredUser()))
+                    .replace(R.id.container, mUsersFragment)
                     .commit();
         }
     }
@@ -121,6 +152,10 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
                 .connect();
     }
 
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
     /**
      * User fragment
      */
@@ -129,7 +164,7 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
         RecyclerView mRecyclerView;
         ChatListAdapter mAdapter;
         String mRegisteredUser;
-        Button mButtonUpdateUser, mButtonCreateGroup;
+        Button mButtonUpdateUser, mButtonChatWith;
 
         public static UsersFragment getInstance(String registeredUserId) {
             UsersFragment fragment = new UsersFragment();
@@ -150,7 +185,7 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
 
         private void loadRecyclerView(View view) {
             mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler);
-            mAdapter = new ChatListAdapter();
+            mAdapter = new ChatListAdapter(mRegisteredUser);
             LinearLayoutManager manager = new LinearLayoutManager(view.getContext(),
                     LinearLayoutManager.VERTICAL, false);
             mRecyclerView.setHasFixedSize(true);
@@ -158,32 +193,78 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
             mRecyclerView.setAdapter(mAdapter);
         }
 
+        public void updateList() {
+            if (mRecyclerView != null && mRecyclerView.getAdapter() != null) {
+                mRecyclerView.getAdapter().notifyDataSetChanged();
+            }
+        }
+
         @Override
         public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
             super.onViewCreated(view, savedInstanceState);
             mButtonUpdateUser = (Button) view.findViewById(R.id.button_update_user);
-            mButtonCreateGroup = (Button) view.findViewById(R.id.button_create_group);
+            mButtonChatWith = (Button) view.findViewById(R.id.button_chat_with_user);
 
             mButtonUpdateUser.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     PepperTalk.getInstance(getActivity())
-                            .updateUser("Jon", null);
+                            .updateUser("Jon", null, new UserCallback() {
+                                @Override
+                                public void onSuccess(com.espreccino.peppertalk.User user) {
+
+                                }
+
+                                @Override
+                                public void onFail(PepperTalkError error) {
+
+                                }
+                            });
                 }
             });
 
-            mButtonCreateGroup.setOnClickListener(new View.OnClickListener() {
+
+            mButtonChatWith.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    String grpId = "grp:" + UUID.randomUUID();
-                    List<String> members = new ArrayList<String>();
-                    members.add("b_android@getpeppertalk.com");
+                    AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
 
-                    PepperTalk.getInstance(getActivity())
-                            .createGroup(grpId,
-                                    "Talk Group",
-                                    null,
-                                    members);
+                    alert.setTitle("Enter an email");
+                    alert.setMessage("Chat with new user");
+
+                    final EditText input = new EditText(v.getContext());
+                    alert.setView(input);
+
+                    alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = input.getText().toString();
+                            boolean has = false;
+                            for (User user : mUsers) {
+                                if (value.equals(user.email)) {
+                                    has = true;
+                                    break;
+                                }
+                            }
+                            if (!has) {
+                                User user = new User(value, value);
+                                mUsers.add(user);
+                            }
+                            PepperTalk.getInstance(getActivity())
+                                    .chatWithParticipant(value)
+                                    .topicId(mTopics.get(0).topicId)
+                                    .topicTitle(mTopics.get(0).topicTitle)
+                                    .start();
+                        }
+                    });
+
+                    alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            dialog.dismiss();
+                        }
+                    });
+
+                    alert.show();
+
                 }
             });
         }
@@ -191,9 +272,12 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
         private class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.UserHolder>
                 implements PepperTalk.MessageListener {
 
-            ChatListAdapter() {
+            String registeredUser;
+
+            ChatListAdapter(String registeredUser) {
+                this.registeredUser = registeredUser;
                 PepperTalk.getInstance(getActivity())
-                        .setMessageListener(this);
+                        .registerMessageListener(this);
             }
 
             @Override
@@ -215,6 +299,16 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
 
             @Override
             public void onNewMessage(String userId, String topicId, int unreadCount) {
+                boolean userExists = false;
+                for (User user : mUsers) {
+                    if (user.email.equals(userId)) {
+                        userExists = true;
+                    }
+                }
+                if (!userExists) {
+                    User user = new User(userId, userId);
+                    mUsers.add(user);
+                }
                 notifyDataSetChanged();
             }
 
@@ -225,6 +319,7 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
                 final TextView textUserEmail;
                 final TextView textMessageCount;
                 User user;
+                String id;
 
                 public UserHolder(View itemView) {
                     super(itemView);
@@ -235,25 +330,31 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
                     textMessageCount = (TextView) itemView.findViewById(R.id.textView_message_count);
                 }
 
-                public void loadUser(User user) {
-                    this.user = user;
-                    textUserEmail.setText(user.email);
-                    textUserName.setText(user.name);
-                    int count = PepperTalk.getInstance(getActivity())
-                            .getParticipantUnreadCount(user.email, mTopics.get(0).topicId);
-                    if (count > 0) {
+                public void loadUser(Object obj) {
+                    if (obj instanceof User) {
+                        this.user = (User) obj;
+                        this.id = user.email;
+                        textUserEmail.setText(user.email);
+                        textUserName.setText(user.name);
+                        if (user.email.equals(registeredUser)) {
+                            textUserEmail.setTextColor(Color.GREEN);
+                            view.setBackgroundColor(Color.RED);
+                        } else {
+                            textUserEmail.setTextColor(Color.BLACK);
+                            view.setBackgroundColor(Color.BLUE);
+                        }
+                        int count = PepperTalk.getInstance(getActivity())
+                                .getParticipantUnreadCount(user.email, mTopics.get(0).topicId);
                         textMessageCount.setText(count + "");
-                    } else {
-                        textMessageCount.setText("");
                     }
                 }
 
                 private class UserClickListener implements View.OnClickListener {
                     @Override
                     public void onClick(View v) {
-                        if (user != null) {
+                        if (id != null) {
                             PepperTalk.getInstance(getActivity())
-                                    .chatWithParticipant(user.email)
+                                    .chatWithParticipant(id)
                                     .topicId(mTopics.get(0).topicId)
                                     .topicTitle(mTopics.get(0).topicTitle)
                                     .start();
@@ -297,4 +398,71 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
         return getSharedPreferences("com.esp", MODE_PRIVATE);
     }
 
+    private String PREF_USERS = "prefs_users";
+    private String PREF_TOPICS = "prefs_topics";
+
+    private void saveDataToSharedPrefs() {
+        Gson gson = new Gson();
+        SharedPreferences.Editor editor = getSharedPrefs().edit();
+
+        Set<String> set = new HashSet<String>();
+        for (User user : mUsers) {
+            set.add(gson.toJson(user));
+        }
+        editor.putStringSet(PREF_USERS, set)
+                .apply();
+
+        set.clear();
+        for (Topic topic : mTopics) {
+            set.add(gson.toJson(topic));
+        }
+        editor.putStringSet(PREF_TOPICS, set)
+                .apply();
+
+    }
+
+    private void loadDataFromPrefs() {
+        Gson gson = new Gson();
+        mUsers = new ArrayList<User>();
+        mTopics = new ArrayList<Topic>();
+
+        SharedPreferences.Editor editor = getSharedPrefs().edit();
+        Set<String> set;
+
+        set = getSharedPrefs().getStringSet(PREF_USERS, Collections.EMPTY_SET);
+        if (set.isEmpty()) {
+            if (mUsers.size() != USERS.length) {
+                for (String usr : USERS) {
+                    String[] split = usr.split(":");
+                    mUsers.add(new User(split[0], split[1]));
+                }
+            }
+        } else {
+            for (String s : set) {
+                User user = gson.fromJson(s, User.class);
+                mUsers.add(user);
+            }
+        }
+
+        set = getSharedPrefs().getStringSet(PREF_TOPICS, Collections.EMPTY_SET);
+
+        if (set.isEmpty()) {
+            if (mTopics.size() != TOPICS.length) {
+                for (String topic : TOPICS) {
+                    String[] split = topic.split(":");
+                    mTopics.add(new Topic(split[0], split[1]));
+                }
+            }
+        } else {
+            for (String s : set) {
+                Topic topic = gson.fromJson(s, Topic.class);
+                mTopics.add(topic);
+            }
+        }
+
+        String userId = getRegisteredUser();
+        if (userId != null) {
+            loadUserFragment(getRegisteredUser());
+        }
+    }
 }
